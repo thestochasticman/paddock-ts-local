225f2e9e7b7957343284441327548b274d5acaed
Yasar <yasar@yasar-m1-macbook-air.local>
2025-05-05 12:20:56 +1000

    eh

--- PaddockTSLocal/SamGeoPaddocks/segment.py
+++ PaddockTSLocal/SamGeoPaddocks/segment.py
@@ -1,9 +1,14 @@
 from PaddockTSLocal.SamGeoPaddocks.download_weights import f as download_weights
 from PaddockTSLocal.SamGeoPaddocks.config import Config
-from samgeo import SamGeo, sam_model_registry
 from PaddockTSLocal.Logger import Logger
 from os.path import exists
+from samgeo import SamGeo
 
-def f(config: Config, path_image: str):
+def f(path_image: str, path_output: str, config: Config):
     if not exists(config.path): download_weights(config)
-    
+    model = SamGeo(model_type=config.type, checkpoint=config.path)
+    model.generate(
+
+    )
+def t(): 
+    pass
