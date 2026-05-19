0bc2879fea02b378f5a97dfc103834e147240dc0
yasar <yasaradeel@gmail.com>
2026-05-19 16:22:37 +1000

    slight change to sentinel pipeline scripts

--- PaddockTS/sentinel2_to_paddockTS_pipeline.py
+++ PaddockTS/sentinel2_to_paddockTS_pipeline.py
@@ -56,6 +56,129 @@ def _make_table(statuses, times):
     return table
 
 
+def _run_pipeline_steps(query, statuses, times, log, update_callback=None):
+    import xarray as xr
+
+    ds_sentinel2 = None
+    ds_fractional_cover = None
+    paddocks = None
+    ds_paddockTS = None
+    ds_yearly = None
+    phenology_results = None
+
+    os.makedirs(query.tmp_dir, exist_ok=True)
+    step_errors = []
+    for i in range(len(STEPS)):
+        # Clean up before segmentation step
+        if i == 4:
+            gc.collect()
+            for var in ['OMP_NUM_THREADS', 'MKL_NUM_THREADS', 'OPENBLAS_NUM_THREADS']:
+                os.environ.pop(var, None)
+            try:
+                import torch
+                torch.set_num_threads(os.cpu_count() or 4)
+                if torch.cuda.is_available():
+                    torch.cuda.empty_cache()
+            except ImportError:
+                pass
+
+        statuses[i] = 'running'
+        if update_callback:
+            update_callback()
+        t0 = time.time()
+        try:
+            with redirect_stdout(log), redirect_stderr(log):
+                if i == 0:
+                    # Download + clean Sentinel-2
+                    if not exists(query.sentinel2_path):
+                        from PaddockTS.Sentinel2.download_sentinel2 import download_sentinel2
+                        ds_sentinel2 = download_sentinel2(query)
+                    else:
+                        ds_sentinel2 = xr.open_zarr(query.sentinel2_path, chunks=None, decode_coords='all')
+                    from PaddockTS.Sentinel2.clean_sentinel2 import clean_sentinel2
+                    ds_sentinel2 = clean_sentinel2(query, ds_sentinel2=ds_sentinel2)
+
+                elif i == 1:
+                    # Compute indices
+                    from PaddockTS.SpectralIndices.indices import compute_indices
+                    ds_sentinel2 = compute_indices(query, ds_sentinel2=ds_sentinel2)
+
+                elif i == 2:
+                    # Compute fractional cover
+                    from PaddockTS.FractionalCover.compute_fractional_cover import compute_fractional_cover
+                    ds_fractional_cover = compute_fractional_cover(query, ds_sentinel2=ds_sentinel2)
+
+                elif i == 3:
+                    # Sentinel-2 video
+                    from PaddockTS.Plotting.sentinel2_video import sentinel2_video
+                    sentinel2_video(query, ds_sentinel2=ds_sentinel2)
+
+                elif i == 4:
+                    # Segment paddocks
+                    import geopandas as gpd
+                    if exists(query.sam_paddocks_path):
+                        paddocks = gpd.read_file(query.sam_paddocks_path)
+                    else:
+                        from PaddockTS.PaddockSegmentation.get_paddocks import get_paddocks
+                        paddocks = get_paddocks(query, ds_sentinel2=ds_sentinel2)
+
+                elif i == 5:
+                    # Sentinel-2 + paddocks video
+                    from PaddockTS.Plotting.sentinel2_paddocks_video import sentinel2_video_with_paddocks
+                    sentinel2_video_with_paddocks(query, query.sam_paddocks_path, ds_sentinel2=ds_sentinel2)
+
+                elif i == 6:
+                    # Fractional cover video
+                    from PaddockTS.Plotting.fractional_cover_video import fractional_cover_video
+                    fractional_cover_video(query, ds_fractional_cover=ds_fractional_cover)
+
+                elif i == 7:
+                    # Fractional cover + paddocks video
+                    from PaddockTS.Plotting.fractional_cover_paddocks_video import fractional_cover_paddocks_video
+                    fractional_cover_paddocks_video(query, query.sam_paddocks_path, ds_fractional_cover=ds_fractional_cover, ds_sentinel2=ds_sentinel2)
+
+                elif i == 8:
+                    # Make paddock time series
+                    from PaddockTS.Phenology.make_paddock_time_series import make_paddock_time_series
+                    ds_paddockTS = make_paddock_time_series(query, ds_sentinel2=ds_sentinel2, paddocks_filepath=query.sam_paddocks_path)
+
+                elif i == 9:
+                    # Make yearly paddock time series
+                    from PaddockTS.Phenology.make_yearly_paddock_time_series import make_yearly_paddock_time_series
+                    ds_yearly = make_yearly_paddock_time_series(query, ds_paddockTS=ds_paddockTS, paddocks_filepath=query.sam_paddocks_path)
+
+                elif i == 10:
+                    # Estimate phenology
+                    from PaddockTS.Phenology.estimate_phenology import estimate_phenology
+                    phenology_results = estimate_phenology(query, ds_yearly=ds_yearly)
+
+                elif i == 11:
+                    # Calendar plot
+                    from PaddockTS.Plotting.calendar_plot import calendar_plot
+                    calendar_plot(query, ds_sentinel2=ds_sentinel2, paddocks_filepath=query.sam_paddocks_path)
+
+                elif i == 12:
+                    # Phenology plot
+                    from PaddockTS.Plotting.phenology_plot import phenology_plot
+                    phenology_plot(query, phenology_results=phenology_results, ds_yearly=ds_yearly, ds_paddockTS=ds_paddockTS)
+
+            statuses[i] = 'done'
+        except Exception as e:
+            statuses[i] = '[red]error[/red]'
+            times[i] = time.time() - t0
+            if update_callback:
+                update_callback()
+            step_errors.append((STEPS[i], e))
+            continue
+        times[i] = time.time() - t0
+        if update_callback:
+            update_callback()
+
+    # If any steps failed, raise the first error
+    if step_errors:
+        raise step_errors[0][1]
+
+
 def run(query: Query, reload: bool = False):
     import xarray as xr
 
@@ -85,13 +208,6 @@ def run(query: Query, reload: bool = False):
     statuses = ['pending'] * len(STEPS)
     times = [None] * len(STEPS)
 
-    ds_sentinel2 = None
-    ds_fractional_cover = None
-    paddocks = None
-    ds_paddockTS = None
-    ds_yearly = None
-    phenology_results = None
-
     progress = Progress(
         TextColumn('[bold blue]{task.description}'),
         BarColumn(),
@@ -105,113 +221,11 @@ def run(query: Query, reload: bool = False):
     log = open(f'{query.tmp_dir}/{query.stub}_sentinel2_to_paddockTS.log', 'w')
 
     with Live(Group(_make_table(statuses, times), progress), console=_console, refresh_per_second=4) as live:
-        for i in range(len(STEPS)):
-            statuses[i] = 'running'
+        def update_display():
+            progress.update(task_id, completed=sum(1 for s in statuses if s in ['done', '[red]error[/red]']))
             live.update(Group(_make_table(statuses, times), progress))
 
-            # Clean up before segmentation step
-            if i == 4:
-                gc.collect()
-                for var in ['OMP_NUM_THREADS', 'MKL_NUM_THREADS', 'OPENBLAS_NUM_THREADS']:
-                    os.environ.pop(var, None)
-                try:
-                    import torch
-                    torch.set_num_threads(os.cpu_count() or 4)
-                    if torch.cuda.is_available():
-                        torch.cuda.empty_cache()
-                except ImportError:
-                    pass
-
-            t0 = time.time()
-            try:
-                with redirect_stdout(log), redirect_stderr(log):
-                    if i == 0:
-                        # Download + clean Sentinel-2
-                        if not exists(query.sentinel2_path):
-                            from PaddockTS.Sentinel2.download_sentinel2 import download_sentinel2
-                            ds_sentinel2 = download_sentinel2(query)
-                        else:
-                            ds_sentinel2 = xr.open_zarr(query.sentinel2_path, chunks=None, decode_coords='all')
-                        from PaddockTS.Sentinel2.clean_sentinel2 import clean_sentinel2
-                        ds_sentinel2 = clean_sentinel2(query, ds_sentinel2=ds_sentinel2)
-
-                    elif i == 1:
-                        # Compute indices
-                        from PaddockTS.SpectralIndices.indices import compute_indices
-                        ds_sentinel2 = compute_indices(query, ds_sentinel2=ds_sentinel2)
-
-                    elif i == 2:
-                        # Compute fractional cover
-                        from PaddockTS.FractionalCover.compute_fractional_cover import compute_fractional_cover
-                        ds_fractional_cover = compute_fractional_cover(query, ds_sentinel2=ds_sentinel2)
-
-                    elif i == 3:
-                        # Sentinel-2 video
-                        from PaddockTS.Plotting.sentinel2_video import sentinel2_video
-                        sentinel2_video(query, ds_sentinel2=ds_sentinel2)
-
-                    elif i == 4:
-                        # Segment paddocks
-                        import geopandas as gpd
-                        gpkg_path = f'{query.tmp_dir}/{query.stub}_sam_paddocks.gpkg'
-                        if exists(gpkg_path):
-                            paddocks = gpd.read_file(gpkg_path)
-                        else:
-                            from PaddockTS.PaddockSegmentation.get_paddocks import get_paddocks
-                            paddocks = get_paddocks(query, ds_sentinel2=ds_sentinel2)
-
-                    elif i == 5:
-                        # Sentinel-2 + paddocks video
-                        from PaddockTS.Plotting.sentinel2_paddocks_video import sentinel2_video_with_paddocks
-                        sentinel2_video_with_paddocks(query, paddocks, ds_sentinel2=ds_sentinel2)
-
-                    elif i == 6:
-                        # Fractional cover video
-                        from PaddockTS.Plotting.fractional_cover_video import fractional_cover_video
-                        fractional_cover_video(query, ds_fractional_cover=ds_fractional_cover)
-
-                    elif i == 7:
-                        # Fractional cover + paddocks video
-                        from PaddockTS.Plotting.fractional_cover_paddocks_video import fractional_cover_paddocks_video
-                        fractional_cover_paddocks_video(query, paddocks, ds_fractional_cover=ds_fractional_cover, ds_sentinel2=ds_sentinel2)
-
-                    elif i == 8:
-                        # Make paddock time series
-                        from PaddockTS.Phenology.make_paddock_time_series import make_paddock_time_series
-                        gpkg_path = f'{query.tmp_dir}/{query.stub}_sam_paddocks.gpkg'
-                        ds_paddockTS = make_paddock_time_series(query, ds_sentinel2=ds_sentinel2, paddocks_filepath=gpkg_path)
-
-                    elif i == 9:
-                        # Make yearly paddock time series
-                        from PaddockTS.Phenology.make_yearly_paddock_time_series import make_yearly_paddock_time_series
-                        ds_yearly = make_yearly_paddock_time_series(query, ds_paddockTS=ds_paddockTS, paddocks_filepath=gpkg_path)
-
-                    elif i == 10:
-                        # Estimate phenology
-                        from PaddockTS.Phenology.estimate_phenology import estimate_phenology
-                        phenology_results = estimate_phenology(query, ds_yearly=ds_yearly)
-
-                    elif i == 11:
-                        # Calendar plot
-                        from PaddockTS.Plotting.calendar_plot import calendar_plot
-                        calendar_plot(query, ds_sentinel2=ds_sentinel2, paddocks=paddocks)
-
-                    elif i == 12:
-                        # Phenology plot
-                        from PaddockTS.Plotting.phenology_plot import phenology_plot
-                        phenology_plot(query, phenology_results=phenology_results, ds_yearly=ds_yearly, ds_paddockTS=ds_paddockTS)
-
-                statuses[i] = 'done'
-            except Exception:
-                statuses[i] = '[red]error[/red]'
-                times[i] = time.time() - t0
-                live.update(Group(_make_table(statuses, times), progress))
-                log.close()
-                raise
-
-            times[i] = time.time() - t0
-            progress.update(task_id, completed=i + 1)
-            live.update(Group(_make_table(statuses, times), progress))
+        _run_pipeline_steps(query, statuses, times, log, update_callback=update_display)
 
     log.close()
 
